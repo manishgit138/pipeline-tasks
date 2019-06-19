c249c102c5d8b5fdbdbaa6d37ed3d4a8658c35ad
patrickcrocker <patrick.crocker@gmail.com>
2019-06-19 13:21:12 -0500

    debug

--- build-maven-semver-cache/task.sh
+++ build-maven-semver-cache/task.sh
@@ -4,7 +4,7 @@
 #
 # All UPERCASE variables are provided externally from this script
 
-set -eu
+set -eux
 set -o pipefail
 
 version=$(cat version/version)
@@ -12,10 +12,10 @@ version=$(cat version/version)
 cd project
 
 args="-Drevision=$version"
-[ -n "$MAVEN_PROJECTS" ] && args="$args --projects $MAVEN_PROJECTS"
-[ -n "$MAVEN_REPO_MIRROR" ] && args="$args -Drepository.url=$MAVEN_REPO_MIRROR";
-[ -n "$MAVEN_REPO_USERNAME" ] && args="$args -Drepository.username=$MAVEN_REPO_USERNAME";
-[ -n "$MAVEN_REPO_PASSWORD" ] && args="$args -Drepository.password=$MAVEN_REPO_PASSWORD";
+# [ -n "$MAVEN_PROJECTS" ] && args="$args --projects $MAVEN_PROJECTS"
+# [ -n "$MAVEN_REPO_MIRROR" ] && args="$args -Drepository.url=$MAVEN_REPO_MIRROR";
+# [ -n "$MAVEN_REPO_USERNAME" ] && args="$args -Drepository.username=$MAVEN_REPO_USERNAME";
+# [ -n "$MAVEN_REPO_PASSWORD" ] && args="$args -Drepository.password=$MAVEN_REPO_PASSWORD";
 [ "true" = "$MAVEN_REPO_CACHE_ENABLE" ] && args="$args -Dmaven.repo.local=$PWD/.m2repository"
 
 ./mvnw install $args
